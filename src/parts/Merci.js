8b094280d22e0df4424bdc533d9d39839ee4951d
maelmonsieur <171589771+maelmonsieur@users.noreply.github.com>
2025-02-13 17:21:46 +0100

    Update Merci.js

--- src/parts/Merci.js
+++ src/parts/Merci.js
@@ -3,6 +3,6 @@ import Bento from "../components/Bento"
 export default function Merci(props) {
     return(
     <Bento className="container">
-        <b>Merci beaucoup</b> d'avoir parcouru mon portfolio jusqu'au bout ! Si mon profil vous intéresse pour une alternance de 2 ans à partir de Septembre 2024 (rythme : 1 semaine cours - 2 semaines entreprise), contactez moi !<br /><br />N'hésitez pas à partager ce site, <b>à très bientôt !</b>
+        <b>Merci beaucoup</b> d'avoir parcouru mon portfolio jusqu'au bout ! Si mon profil vous intéresse pour une alternance de 2 ans à partir de Septembre 2025 (rythme : 1 semaine cours - 2 semaines entreprise), contactez moi !<br /><br />N'hésitez pas à partager ce site, <b>à très bientôt !</b>
     </Bento>)
 }
